b3ff26835c1a15138b1e89a267e2e293788ce692
Kent Inverarity <kinverarity@hotmail.com>
2022-05-19 11:00:13 +0930

    Update docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 project_name = "korexo_profile"
 
 project = f"{project_name} documentation"
-copyright = "2021 DEW"
+copyright = "2021-2022 DEW"
 author = "DEW Water Science (Kent Inverarity)"
 
 from pkg_resources import get_distribution
@@ -45,9 +45,9 @@ html_theme_options = {
 
 html_context = {
     'display_github': False,
-    # 'github_user': 'dew-waterscience',
-    # 'github_repo': 'dew_gwdata',
-    # 'github_version': f'v{release}/' 
+    'github_user': 'dew-waterscience',
+    'github_repo': 'korexo_profile',
+    'github_version': f'v{release}/' 
 }
 
 html_static_path = ["_static"]
@@ -63,42 +63,3 @@ intersphinx_cache_limit = 1
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-
-# def setup(app):
-#     app.add_css_file("custom.css")
-
-
-
-# project = 'sageodata-db'
-# copyright = '2021, DEW Water Science (Kent Inverarity)'
-# author = 'DEW Water Science (Kent Inverarity)'
-
-
-# # -- General configuration ---------------------------------------------------
-
-# # Add any Sphinx extension module names here, as strings. They can be
-# # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# # ones.
-# extensions = [
-# ]
-
-# # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
-
-# # List of patterns, relative to source directory, that match files and
-# # directories to ignore when looking for source files.
-# # This pattern also affects html_static_path and html_extra_path.
-# exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# # -- Options for HTML output -------------------------------------------------
-
-# # The theme to use for HTML and HTML Help pages.  See the documentation for
-# # a list of builtin themes.
-# #
-# html_theme = 'alabaster'
-
-# # Add any paths that contain custom static files (such as style sheets) here,
-# # relative to this directory. They are copied after the builtin static files,
-# # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
